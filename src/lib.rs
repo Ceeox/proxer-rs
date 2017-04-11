7f8f3cf80de955b7cffcb6109fbcb92e4e462264
souryo <dev.souryo@gmail.com>
2017-04-12 01:27:05 +0200

    updated rust version

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! Die meisten Dokumentationen sind aus dem Proxer-API wiki.
 //! Sollte etwas in der crate fehlen, oder anderweitig nicht stimmen, bitte auf Github melden.
 //!
-//! Aktuell nur auf nighly verfügbar.
+//! Aktuell unterstützte Rust Version >= 1.15
 //!
 //! # Auszug aus dem Proxer-API wiki
 //!
